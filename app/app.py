b205ac4131f88b756d2da5c05204bb13149ef372
Max Lang <maxwell.g.lang@gmail.com>
2015-07-12 14:16:19 -0700

    focus on db

--- app/app.py
+++ app/app.py
@@ -9,14 +9,6 @@ def index(section='home'):
 def error404(error):
     return 'This is not the page you are looking for.'
 
-@get('/static/<filepath:path>')
-def static(filepath):
-    return static_file(filepath, root='static')
-    
-@get('/bower_components/<filepath:path>')
-def bower_files(filepath):
-    return static_file(filepath, root='bower_components')
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 1337))
     run(host='0.0.0.0', port=port, debug=True, reloader=True)
